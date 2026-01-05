388ce2e54922f830ddb3dc2ab2a7b074dce5f99b
GhadyAbuRaya <vxytjghx7d@privaterelay.appleid.com>
2026-01-05 20:27:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import joblib
 
+df = pd.read_csv('parkinsons.csv')
+df.head()
+
+df.columns
+selected_features = ["D2", "PPE"]
+x = df[selected_features]
+y = df["status"]
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = KNeighborsClassifier(n_neighbors=4)
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+
+joblib.dump(model, 'my_model.joblib')
